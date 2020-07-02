476970e01655e292610555360dfe53bdf109858a
Francisco Magno <flmagnom@gmail.com>
2020-07-02 18:18:39 +0100

    fixes

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ window.addEventListener("load", function () {
   // $("#modals").load("modals.html");
 
   $("#luis").on("click", function () {
-    console.log("clicked!");
+    // console.log("clicked!");
     $("#luisModal").modal("show");
+  });
+
+  $("#miguel").on("click", function () {
+    // console.log("clicked!");
+    $("#miguelModal").modal("show");
+  });
+
+  $("#mara").on("click", function () {
+    // console.log("clicked!");
+    $("#maraModal").modal("show");
+  });
+
+  $("#iolanda").on("click", function () {
+    // console.log("clicked!");
+    $("#iolandaModal").modal("show");
   });
 });
